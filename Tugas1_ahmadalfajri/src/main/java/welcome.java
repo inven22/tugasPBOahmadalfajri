9214589106e9360f38ecc22c37f7bde16049ddc7
ahmad al fajri <116804256+inven22@users.noreply.github.com>
2023-08-25 21:25:40 +0700

    Add files via upload

---
+++ Tugas1_ahmadalfajri/src/main/java/welcome.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ahmad al fajri
+ */
+public class welcome {
+    public static void main(String[] args)
+    {
+    String greeting = "welcome to java";
+    System.out.println(greeting);
+    for (int i =0;i < greeting.length(); i++)
+        System.out.println("=");
+    System.out.println();
+    }
+}
---
+++ Tugas1_ahmadalfajri/welcome.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public class welcome
+{
+    public static void main(String[] args)
+    {
+    String greeting = "welcome to java";
+    System.out.println(greeting);
+    for (int i =0;i < greeting.length(); i++)
+        System.out.println("=");
+    System.out.println();
+    }
+}
